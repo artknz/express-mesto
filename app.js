72abc312ac7435201b4ce0cfe7c4cef9b189643a
Artyom Knyazev <a.knyazev7@yandex.ru>
2020-10-29 09:08:50 +0300

    feat: добавление api

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const path = require('path')
+const userRoutes = require('./routes/users.js')
+const cardsRoutes = require('./routes/cards.js')
+const errorRoutes = require('./routes/error.js')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', userRoutes)
+app.use('/', cardsRoutes)
+app.use('/', errorRoutes)
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
